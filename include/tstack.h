87d7c4ed62a2155c15c3542efbfc8fe880c1fc08
belov <107872776+user122664623@users.noreply.github.com>
2024-04-14 21:45:36 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,43 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  int currentFreePosition = 0;
+  T data[size];
+
+ public:
+  void push(T element) {
+  if (currentFreePosition == size) {
+      return;
+  }
+  data[currentFreePosition] = element;
+  currentFreePosition++;
+  }
+  T pop() {
+  if (currentFreePosition == 0) {
+      throw std::exception();
+  }
+  currentFreePosition--;
+  return data[currentFreePosition];
+  }
+  T seeTop() {
+  if (currentFreePosition == 0) {
+      throw std::exception();
+  }
+  return data[currentFreePosition - 1];
+  }
+  T seeBottom() {
+  if (currentFreePosition == 0) {
+      throw std::exception();
+  }
+  return data[0];
+  }
+  bool isEmpty() {
+  if (currentFreePosition == 0) {
+      return true;
+  }
+  return false;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
